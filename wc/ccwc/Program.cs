c57796454507bab293a65981dcd93cef6e523337
Paul Garton <paulgarton1984@hotmail.com>
2026-04-06 19:29:32 +0100

    step one ccwc

--- wc/ccwc/Program.cs
+++ wc/ccwc/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace ccwc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Step 1
+        string flag = args[0];
+        string fileName = args[1];
+        
+        if (flag == "-c")
+        {
+            byte[] bytes = File.ReadAllBytes(fileName);
+            Console.WriteLine($"{bytes.Length} {fileName}");
+        }
+    }
+}
